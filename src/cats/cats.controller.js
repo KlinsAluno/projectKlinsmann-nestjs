1738b4cd984072652fd8358e02deb84f95391e95
Pedrix55 <pedrogabriel12gamer@outlook.com>
2023-08-01 15:37:20 -0300

    Array GATOS >> controller.cats

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+const GATOS = [
+  {
+    id: 1,
+    nome: 'Franeudos',
+    corOlhos: 'verde',
+    raca: 'sphinx',
+  },
+
+  {
+    id: 2,
+    nome: 'Chico',
+    corOlhos: 'azul',
+    raca: 'siamês',
+  },
+
+  {
+    id: 3,
+    nome: 'Bizu',
+    corOlhos: 'castanho',
+    raca: 'rasga-lixo',
+  },
+];
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Get()
+  findAll() {
+    return GATOS;
+  }
+}

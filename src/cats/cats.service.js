2f5b9ff40f53d920fa839ab74a6e9649cc9663ae
KlinsmannAluno <pedrogabriel12gamer@outlook.com>
2023-08-15 16:30:05 -0300

    ajusta a rota DELETE /cats/:id para usar serviço.

--- src/cats/cats.service.js
+++ src/cats/cats.service.js
@@ -15,7 +15,16 @@ export class CatsService {
     // GATOS.find((gato) => gato.id == params.id);
   }
 
+  findIndexById(id) {
+    return this.cats.findIndexById((cat) => cat.id == id);
+  }
+
   create(cat) {
     this.cats.push(cat);
+  }
+
+  deleteByIndex(index) {
+    this.cats.splice(index, 1);
+    // GATOS.splice(indexGatoEncontrado, 1);
   }
 }
